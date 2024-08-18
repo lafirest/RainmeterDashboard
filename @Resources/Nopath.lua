978fdf17b83183482d34558b96f1024f01644c93
firest <blankalupo@163.com>
2024-08-19 04:18:02 +0800

    init

---
+++ @Resources/Nopath.lua
@@ -0,0 +1,14 @@
+PROPERTIES ={
+
+}
+function Initialize()
+   MeasureImageName = SKIN:GetMeasure('Random')
+end
+
+function Update()
+   local FullImageName = MeasureImageName:GetStringValue()
+   local Path, ImageName, Extension = string.match(FullImageName, '(.-)([^\\]-([^%.]+))$')
+   
+   SKIN:Bang('!SetOption', 'MeterImageName', 'Text', ImageName)
+   return ImageName
+end
